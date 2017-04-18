504f5896f133b753951d55367562c433eab5285a
Suporte <Suporte@DESKTOP-5F4O40T>
2017-04-18 15:29:18 -0300

    Implementado as classes PersistenceUsuario e PersistenceProduto

--- SmartGondula/src/br/com/gfsoft/smartgondula/persistence/PersistenceProduto.java
+++ SmartGondula/src/br/com/gfsoft/smartgondula/persistence/PersistenceProduto.java
@@ -1,38 +1,193 @@
 package br.com.gfsoft.smartgondula.persistence;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.swing.JOptionPane;
+
+import com.mysql.jdbc.Statement;
+
 import br.com.gfsoft.smartgondula.model.Produto;
+import br.com.gfsoft.smartgondula.model.Usuario;
 
 public class PersistenceProduto implements IPersistenceProduto {
+	
+	private static Statement stmt;
+	private static ResultSet rs;
+	private static Conexao con = new Conexao();
 
 	@Override
 	public boolean insert(Produto produto) {
-		// TODO Auto-generated method stub
+		String sql = "INSERT INTO tb_produto(codigo, descricao, preco) "
+				+ "VALUES('"+ produto.getCodigo() +"',"
+						+ "'"+ produto.getDescricao() +"',"
+						+ produto.getPreco() +");";
+		
+		try {
+			con.getConnection().createStatement().executeUpdate(sql);
+			return true;
+		} catch (SQLException ex) {
+			// Excecao para banco de dados
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro na inserção dos dados na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public boolean delete(Produto produto) {
-		// TODO Auto-generated method stub
+		String sql;
+		
+		sql = "DELETE FROM tb_produto WHERE codigo = "+ produto.getCodigo() + ";";
+		
+		try {
+			con.getConnection().createStatement().executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao deletar dados na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		
 		return false;
 	}
 
 	@Override
 	public boolean update(Produto produto) {
-		// TODO Auto-generated method stub
+		
+		String sql = "UPDATE tb_produto SET descricao = '"+produto.getDescricao()+"',"
+				+ " preco = '"+produto.getPreco()+"' "
+																				
+				+ " WHERE codigo = "+produto.getCodigo()+";";
+		
+		try {
+			con.getConnection().createStatement().executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public Produto selectProduto(String descricao) {
-		// TODO Auto-generated method stub
+		
+		Produto produto = new Produto();
+		String sql = "SELECT * FROM tb_produto WHERE descricao='" + descricao + "';";
+		
+		try {
+			stmt = (Statement) con.getConnection().prepareStatement(sql);
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				produto.setCodigo(rs.getInt("codigo"));
+				produto.setDescricao(rs.getString("descricao"));
+				produto.setPreco(rs.getFloat("preco"));
+			}
+			
+			return produto;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return null;
 	}
 
 	@Override
 	public Set<Produto> selectProdutos() {
-		// TODO Auto-generated method stub
+		
+		Set<Produto> produtos = new HashSet<>();
+		Produto produto;
+		
+		try {
+			stmt = (Statement) con.getConnection().createStatement();
+			String sql = "SELECT * FROM tb_produto;";
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				produto = new Produto();
+				
+				produto.setCodigo(rs.getInt("codigo"));
+				produto.setDescricao(rs.getString("descricao"));
+				produto.setPreco(rs.getFloat("preco"));
+				
+				produtos.add(produto);
+			}
+			
+			return produtos;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro na busca dos produtos na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return null;
 	}
 
--- SmartGondula/src/br/com/gfsoft/smartgondula/persistence/PersistenceUsuario.java
+++ SmartGondula/src/br/com/gfsoft/smartgondula/persistence/PersistenceUsuario.java
@@ -1,39 +1,196 @@
 package br.com.gfsoft.smartgondula.persistence;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Set;
+
+import javax.swing.JOptionPane;
+
+import com.mysql.jdbc.Statement;
 
 import br.com.gfsoft.smartgondula.model.Usuario;
 
 public class PersistenceUsuario implements IPersistenceUsuario {
+	
+	private static Statement stmt;
+	private static ResultSet rs;
+	private static Conexao con = new Conexao();
 
 	@Override
 	public boolean insert(Usuario usuario) {
-		// TODO Auto-generated method stub
+		
+		String sql = "INSERT INTO tb_usuario(matricula, nome, senha) "
+				+ "VALUES('"+ usuario.getMatricula() +"',"
+						+ "'"+ usuario.getNome() +"',"
+						+ usuario.getSenha() +");";
+		
+		try {
+			con.getConnection().createStatement().executeUpdate(sql);
+			return true;
+		} catch (SQLException ex) {
+			// Excecao para banco de dados
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro na inserção dos dados na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public boolean delete(Usuario usuario) {
-		// TODO Auto-generated method stub
+		
+		String sql;
+		
+		sql = "DELETE FROM tb_usuario WHERE matricula = "+usuario.getMatricula()+";";
+		
+		try {
+			con.getConnection().createStatement().executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao deletar dados na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		
 		return false;
 	}
 
 	@Override
 	public boolean update(Usuario usuario) {
-		// TODO Auto-generated method stub
+		
+		String sql = "UPDATE tb_usuario SET nome = '"+usuario.getNome()+"',"
+				+ " senha = '"+usuario.getSenha()+"' "
+																				
+				+ " WHERE matricula = "+usuario.getMatricula()+";";
+		
+		try {
+			con.getConnection().createStatement().executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public Usuario selectUsuario(String nome) {
-		// TODO Auto-generated method stub
+		
+		Usuario usuario = new Usuario();
+		String sql = "SELECT * FROM tb_usuario WHERE nome='" + nome + "';";
+		
+		try {
+			stmt = (Statement) con.getConnection().prepareStatement(sql);
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				usuario.setMatricula(rs.getLong("matricula"));
+				usuario.setNome(rs.getString("nome"));
+				usuario.setSenha(rs.getString("senha"));
+			}
+			
+			return usuario;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return null;
 	}
 
 	@Override
 	public Set<Usuario> selectUsuarios() {
-		// TODO Auto-generated method stub
+		
+		Set<Usuario> usuarios = new HashSet<>();
+		Usuario usuario;
+		
+		try {
+			stmt = (Statement) con.getConnection().createStatement();
+			String sql = "SELECT * FROM tb_usuario;";
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				usuario = new Usuario();
+				
+				usuario.setMatricula(rs.getLong("matricula"));
+				usuario.setNome(rs.getString("nome"));
+				usuario.setSenha(rs.getString("senha"));
+				
+				usuarios.add(usuario);
+			}
+			
+			return usuarios;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro na busca dos usuários na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+		} finally {
+			try {
+//				if(con != null)
+//					con.getConnection().close();
+				
+				if(stmt != null)
+					stmt.close();
+				
+				if(rs != null)
+					rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return null;
+		
 	}
 
 }
